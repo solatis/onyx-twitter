1d0fab9382a3606fc6780dcf871ddab81ccffe11
Michael Drogalis <mjd3089@rit.edu>
2017-04-12 08:05:36 -0700

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-twitter "0.10.0.0-beta11"
+(defproject org.onyxplatform/onyx-twitter "0.10.0.0-SNAPSHOT"
   :description "Onyx plugin for Twitter"
   :url "https://github.com/onyx-platform/onyx-twitter"
   :license {:name "Eclipse Public License"
